5fd5f06925ac1cd71323877cccc4e13693d22c1d
Maciej Zoltowski <maciej.zoltowski@e-qsg.com>
2019-03-08 11:04:50 +0100

    v1.0.2 CommonModule

--- src/app/modules/hero/hero.module.ts
+++ src/app/modules/hero/hero.module.ts
@@ -1,7 +1,11 @@
 import {NgModule} from '@angular/core';
 import {SelectedHeroComponent} from './components/selected-hero/selected-hero.component';
+import {CommonModule} from '@angular/common';
 
 @NgModule({
+    imports: [
+        CommonModule,
+    ],
     declarations: [
         SelectedHeroComponent
     ],
